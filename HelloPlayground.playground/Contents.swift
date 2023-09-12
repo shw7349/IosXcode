8999da7509230d8edbfc843f4741c9a5cfb071ca
HyunWoo <shw7349@naver.com>
2023-09-12 22:12:24 +0900

    Day1 - 스위프트 초기세팅  2023.09.12

--- HelloPlayground.playground/Contents.swift
+++ HelloPlayground.playground/Contents.swift
@@ -4,6 +4,14 @@ var greeting = "Hello, swift"
 
 print(greeting)
 print(greeting)
-print(greeting)
+
+UIImage(systemName: "star")
+var sum = 0
+for i in 1...10 {
+    sum += i
+}
+print(sum)
+
+
 
 
