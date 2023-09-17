8673b0be7d55432840d15d22763e40bfe394e3b5
HyunWoo <shw7349@naver.com>
2023-09-17 20:00:04 +0900

    Day4- Xcode 기능공부 2023.09.17(if문)

--- HelloXcode/HelloXcode/ViewController.swift
+++ HelloXcode/HelloXcode/ViewController.swift
@@ -15,7 +15,34 @@ class ViewController: UIViewController {
         var a = 10
         a = 20
         print(a)
+        
+        // if문 예시
+        
+        let num = -1
+        
+        if num >= 0
+        {
+            print ("Positive nu,ber or Zero")
+        }
+        else if num % 2 == 0
+        {
+            print("Positive even number")
+        }
+        else if num % 2 == 1
+        {
+            print("Positive odd number")
+        }
+        else
+        {
+            print("negative number")
+        }
+        
+        
     }
+    
+    
+    
+    
 
 
 }
